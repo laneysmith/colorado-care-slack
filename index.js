798f91864d323cf90e8967770dec3fb0ab0458a0
Lee <leeblazek@gmail.com>
2016-07-17 21:00:00 -0600

    remove api token

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// xoxb-59226608148-pcFuQX7DjRZu6Qnk9Cey9Xy0
-
 var token = process.env.SLACK_API_TOKEN || '';
 var RtmClient = require('@slack/client').RtmClient;
 var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
