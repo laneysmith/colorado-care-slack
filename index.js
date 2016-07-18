8bcb8e0b3ef8dadc409915b93dcddba31afd22c5
Lucas Barbula <lucas.barbula@gmail.com>
2016-07-18 08:06:41 -0600

    working

--- index.js
+++ index.js
@@ -13,25 +13,7 @@ let port = process.env.PORT || 3000;
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
-// <<<<<<< HEAD
-// rtm.on(RTM_EVENTS.MESSAGE, function (message) {
-//   // Listens to all `message` events from the team
-//   // { type: 'message',
-//   //   channel: 'C1R69C2G0',
-//   //   user: 'U1R69C14G',
-//   //   text: 'dsf',
-//   //   ts: '1468385402.000013',
-//   //   team: 'T1R6ELRHV' }
-//   console.log(message.text, message.user);
-//   var user = rtm.dataStore.getUserById(message.user)
-//
-//    var dm = rtm.dataStore.getDMByName(user.name);
-//
-//    rtm.sendMessage('Hello ' + user.name + '!', dm.id);
-//   // rtm.sendMessage('Hey you', 'C1R69C2G0', function messageSent() {
-//   //   // optionally, you can supply a callback to execute once the message has been sent
-//   // });
-// =======
+
 
 
 app.post('/commands/motivate', urlencodedParser, function (req, res) {
