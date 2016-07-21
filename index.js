45a0612c10ad8dd843b46a94a5cd3c76eb9cbc39
Lee Blazek <leeblazek@gmail.com>
2016-07-21 15:16:41 -0600

    filter pt1

--- index.js
+++ index.js
@@ -22,14 +22,50 @@ app.get('/', function (req, res) {
   });
 });
 //Call a / command that queries the calander to retrieve events.
+console.log(new Date());
+function sortEvents(startDate) {
+  console.log(startDate);
+  publicGoogleCalendar.getEvents(function(err, events) {
+    if (err) { return console.log(err.message); }
+    // events is now array of all calendar events
+    //start
+    console.log('init length ' + events.length);
+    let filtered = events.filter(function (event) {
+      let today = startDate.getDate();
+      let thisMonth = startDate.getMonth();
+      let day = event.start.getDate();
+      let month = event.start.getMonth();
+      // console.log( day + ' ' + month);
+      if(day === today && month === thisMonth) {
+        return event;
+
+      }
+
+    })
+    console.log(`there are ${filtered.length}
+
+      events`);
+
+    // res.send(events[0]);
+  });
+}
+console.log(sortEvents(new Date()));
 
 app.post('/commands/motivate', urlencodedParser, function (req, res) {
   if (!req.body) return res.sendStatus(400)
   // res.send('Lets do this! hello, hello, test 1 2' + req.body.text);
+
   switch (req.body.text) {
     case 'today':
 
-      res.send('today')
+      // sortEvents(new Date();)
+
+      res.send(`today there are ${events.length} events.
+
+        asds`);
+
+
+
       break;
     case 'week':
 
